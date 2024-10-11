239d494df34f621169c25ac4be18363c5420c255
Antonin Meudic <antonin.meudic@eleve.ensai.fr>
2024-10-11 10:26:52 +0200

    chg init data

--- data/init_db.sql
+++ data/init_db.sql
@@ -1,9 +1,9 @@
 -----------------------------------------------------
--- Joueur
+-- Match
 -----------------------------------------------------
-DROP TABLE IF EXISTS joueur CASCADE ;
-CREATE TABLE joueur(
-    id_joueur    SERIAL PRIMARY KEY,
+DROP TABLE IF EXISTS match CASCADE ;
+CREATE TABLE match (
+    id_match    SERIAL PRIMARY KEY,
     date       VARCHAR(50),
     event          VARCHAR(256),
     blue_team           VARCHAR(50),
@@ -15,3 +15,5 @@ CREATE TABLE joueur(
     orange_players2          VARCHAR(50),
     orange_players3          VARCHAR(50)
 );
+
+
