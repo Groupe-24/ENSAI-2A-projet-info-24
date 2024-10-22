378bcaa1d8b8b037e5014c7fe68ccb7ea20359b0
Hiba Karim <hiba.karim@eleve.ensai.fr>
2024-10-22 10:22:44 +0200

    UtilisateurService

--- src/service/utilisateur_service.py
+++ src/service/utilisateur_service.py
@@ -1,32 +1,33 @@
 from business_object.users.utilisateur import Utilisateur
-from dao.utilisateur_dao import UtilisateurDAO
 
 
 class UtilisateurService:
+    def __init__(self, utilisateurDao):
+        self.utilisateurDao = utilisateurDao
 
     def creer_compte(self, pseudo, nom, mail, ddn, mdp):
         utilisateur = Utilisateur(pseudo, nom, mail, ddn, mdp, False, False)
-        UtilisateurDAO.insert_utilisateur(pseudo, nom, mail, ddn, mdp)
+        self.utilisateurDao.insert_utilisateur(pseudo, nom, mail, ddn, mdp)
         return utilisateur
 
     def se_connecter_utilisateur(self, pseudo, mdp):
-        utili = UtilisateurDAO.get_utilisateur_by_id(pseudo)
-        if utili.pseudo == pseudo:
+        utili = self.utilisateurDao.get_utilisateur_by_id(pseudo)
+        if utili and utili.pseudo == pseudo:
             return utili.mdp == mdp
         return False
 
     def supprimer_utilisateur(self, utilisateur):
-        return self.UtilisateurDAO.delete_utilisateur(utilisateur.pseudo)
+        return self.utilisateurDao.delete_utilisateur(utilisateur.pseudo)
 
     def se_connecter_administrateur(self, pseudo, mdp):
-        utili = UtilisateurDAO.get_utilisateur_by_id(pseudo)
-        if utili.administrateur:
+        utili = self.utilisateurDao.get_utilisateur_by_id(pseudo)
+        if utili and utili.administrateur:
             if utili.pseudo == pseudo:
                 return utili.mdp == mdp
         return False
 
     def se_connecter_organisateur(self, pseudo, mdp):
-        utili = UtilisateurDAO.get_utilisateur_by_id(pseudo)
+        utili = self.utilisateurDao.get_utilisateur_by_id(pseudo)
         if utili.organisateur:
             if utili.pseudo == pseudo:
                 return utili.mdp == mdp
