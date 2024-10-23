a3b19d7e9c53ce97e935714e22222da4de2bfe48
MaximeFerret <maximeferret3@gmail.com>
2024-10-23 12:10:27 +0200

    TournoiService

--- src/service/tournoi_service.py
+++ src/service/tournoi_service.py
@@ -1,5 +1,5 @@
 import uuid
-from business_object.users.tournoi import Tournoi
+from business_object.tournoi import Tournoi
 
 
 class TournoiService:
@@ -8,23 +8,60 @@ class TournoiService:
 
     def creer_tournoi(
         self,
-        date,
-        nom_tournoi,
-        taille_equipe,
-        nb_equipes,
-        prix,
-        equipes=None,
-        matchs=None,
+        id_tournoi=None,
+        titre=None,
+        description=None,
+        date_debut=None,
+        date_fin=None,
         organisateur=None,
     ):
         id_tournoi = str(uuid.uuid4())
         tournoi = Tournoi(
             id_tournoi=id_tournoi,
-            nom=nom_tournoi,
-            nb_equipes=nb_equipes,
-            taille_equipe=taille_equipe,
-            prix=prix,
-            equipes=equipes,
+            titre=titre,
+            description=description,
+            date_debut=date_debut,
+            date_fin=date_fin,
+            organisateur=organisateur.pseudo,
         )
-
+        self.tournoi_dao.insert_tournoi(
+            id_tournoi=id_tournoi,
+            titre=titre,
+            description=description,
+            date_debut=date_debut,
+            date_fin=date_fin,
+            organisateur=organisateur.pseudo,
+        )
         return tournoi
+
+    def lister_tournois(self):
+        resultat = self.tournoi_dao.list_tournois()
+        liste_tournois = []
+        if resultat:
+            for un_tournoi in resultat:
+                tournoi = Tournoi(
+                    id_tournoi=un_tournoi["id_tournoi"],
+                    titre=un_tournoi["titre"],
+                    description=un_tournoi["description"],
+                    date_debut=un_tournoi["date_debut"],
+                    date_fin=un_tournoi["date_fin"],
+                    organisateur=un_tournoi["id_organisateur"],
+                )
+                liste_tournois.append(tournoi)
+        return liste_tournois
+
+    def rechercher_tournoi_nom(self, nom):
+        resultat = self.tournoi_dao.tournoi_par_nom(nom)
+        liste_tournois = []
+        if resultat:
+            for un_tournoi in resultat:
+                tournoi = Tournoi(
+                    id_tournoi=un_tournoi["id_tournoi"],
+                    titre=un_tournoi["titre"],
+                    description=un_tournoi["description"],
+                    date_debut=un_tournoi["date_debut"],
+                    date_fin=un_tournoi["date_fin"],
+                    organisateur=un_tournoi["id_organisateur"],
+                )
+                liste_tournois.append(tournoi)
+        return liste_tournois
