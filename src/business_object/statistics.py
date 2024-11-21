a93cce7941ec5ccc3e127a51295b232f7a00cf73
Hiba Karim <hiba.karim@eleve.ensai.fr>
2024-11-21 14:54:37 +0100

    statistiqueService V4

--- src/business_object/statistics.py
+++ src/business_object/statistics.py
@@ -2,29 +2,36 @@ class Statistics:
     def __init__(
         self,
         id_statistique,
-        equipe,
-        match,
         joueur,
-        but=None,
-        score_de_match=None,
-        consommation_boost=None,
+        match,
+        equipe,
+        goals=None,
+        assists=None,
+        saves=None,
+        shots=None,
+        score=None,
     ):
         """Constructeur"""
         self.id_statistique = id_statistique
-        self.equipe = equipe
-        self.match = match
         self.joueur = joueur
-        self.but = but if but is not None else {}
-        self.score_de_match = score_de_match if score_de_match is not None else {}
-        self.consommation_boost = consommation_boost if consommation_boost is not None else {}
+        self.match = match
+        self.equipe = equipe
+        self.goals = goals if goals is not None else 0  # Défaut à 0 si None
+        self.assists = assists if assists is not None else 0
+        self.saves = saves if saves is not None else 0
+        self.shots = shots if shots is not None else 0
+        self.score = score if score is not None else 0
 
     def __str__(self):
         """Permet d'afficher les informations d'un match avec les statistiques des joueurs"""
-        score_line = f"{self.equipe}: {sum(self.score_de_match)}"
         return (
-            f"Statistique ID :{self.id_statistique}\n"
+            f"Statistique ID : {self.id_statistique}\n"
+            f"Joueur: {self.joueur}\n"
             f"Match ID: {self.match}\n"
-            f"Score:\n{score_line}"
-            f"Statistiques de {self.equipe}:\n"
-            f"Buts: {self.but}\n"
+            f"Equipe: {self.equipe}\n"
+            f"Buts: {self.goals}\n"
+            f"Assists: {self.assists}\n"
+            f"Saves: {self.saves}\n"
+            f"Shots: {self.shots}\n"
+            f"Score: {self.score}"
         )
